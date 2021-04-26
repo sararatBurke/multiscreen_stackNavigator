3852ad5e5c77fcf0e0dfa4744066b10b173b94be
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-25 21:15:11 -0400

    created homeStack

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navigator from './navigation/navigation';
+// import Navigator from './navigation/navigation';
+import Navigator from './routes/homeStack';
 
 export default function App() {
   return (
